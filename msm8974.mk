50c3b21192c97f9b5eb8ba396e256e75eb9b6f6f
Max Weffers <rcstar6696@gmail.com>
2017-08-25 16:53:57 +0200

    msm8974: diplay: Add renderscript package
    Change-Id: I8e39e212a0dc69e4989e1469ff5643dce950e176

--- msm8974.mk
+++ msm8974.mk
@@ -50,6 +50,7 @@ PRODUCT_PACKAGES += \
 	android.hardware.graphics.allocator@2.0-impl \
 	android.hardware.graphics.mapper@2.0-impl \
 	android.hardware.graphics.composer@2.1-impl \
+	android.hardware.renderscript@1.0-impl \
     hwcomposer.msm8974 \
     gralloc.msm8974 \
     copybit.msm8974 \
